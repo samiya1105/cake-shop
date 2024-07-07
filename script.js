323ae5c723fc02ff79bc2e23b191f4e4670207f0
anamaqeel25 <ansarianam030@gmail.com>
2024-07-07 21:16:03 +0530

    modified

--- script.js
+++ script.js
@@ -9,31 +9,3 @@ function toggletheme(){
     }
 }
 
-// Initialize with light theme
-// document.body.style.backgroundColor = "white";
-// document.body.style.color = "black";
-// }
-
-// document.getElementById("toggletheme").onclick = function() {
-//     const currentTheme = document.body.getAttribute("data-theme");
-//     if (currentTheme === "dark") {
-//         setLightTheme();
-//     } else {
-//         setDarkTheme();
-//     }
-// };
-
-// function setLightTheme() {
-//     document.body.style.backgroundColor = "white";
-//     document.body.style.color = "black";
-//     document.body.setAttribute("data-theme", "light");
-// }
-
-// function setDarkTheme() {
-//     document.body.style.backgroundColor = "black";
-//     document.body.style.color = "white";
-//     document.body.setAttribute("data-theme", "dark");
-// }
-
-// // Initialize with light theme
-// setLightTheme();
