9301a9904de0b68d3fcf8ac2ed7e82a9989d3502
anamaqeel25 <ansarianam030@gmail.com>
2024-07-09 17:26:38 +0530

    modified

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 function toggletheme(){
-    var ThemeColor = document.body;
-    if (ThemeColor.style.backgroundColor === "black") {
+    var ThemeColor=document.querySelector('body');
+    var bodyClr = ThemeColor.style.backgroundColor;
+    var logo=document.querySelector('.logo-clr');
+    if ( bodyClr=== "black") {
         ThemeColor.style.backgroundColor = "white";
         ThemeColor.style.color = "black";
+        logo.style.color = "black";
+        // for(var i = 0; i < logo.length; i++){
+        //   logo[i].style.backgroundColor = '#f4f4f4';
+        // }     
     } else {
         ThemeColor.style.backgroundColor = "black";
         ThemeColor.style.color = "white";
+        logo.style.color = "white";
     }
 }
-
